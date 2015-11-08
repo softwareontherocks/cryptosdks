c7b27f8474f92b81e8fd882b0bd2bcdcf07c66d2
Carlos Afonso Pérez <cafonso@e-valua.es>
2015-11-08 21:19:44 +0100

    Add API documentation to the repo page

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Sotr\\Crypto\\AbstractApi"],["c","Sotr\\Crypto\\Bitstamp\\BitstampApi"],["c","Sotr\\Crypto\\Btce\\BtceApi"],["c","Sotr\\Crypto\\Btce\\BtceCurrencyPairResolver"],["c","Sotr\\Crypto\\CurrencyPair"],["c","Sotr\\Crypto\\CurrencyPairResolverInterface"],["c","Sotr\\Crypto\\ExchangeApiInterface"],["c","Sotr\\Crypto\\Ticker"]];
